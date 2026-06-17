7d6436e55e55d3f2d24c0ec9d932def97dacafb9
Littmarz <zdima1612@mail.ru>
2026-06-17 11:32:10 +0800

    ИЗМЕНЕНО: чтение файла ДОБАВЛЕНО: чтение ключа отдельной функцией, заглушка для функции шифрования XOR

--- cipher.cpp
+++ cipher.cpp
@@ -2,12 +2,34 @@
 #include <fstream>
 using namespace std;
 
-void readFile(const string& path) {
-    ifstream file(path, ios::binary);
-    char ch;
-    while (file.get(ch)) {
-        cout << ch;
+string readFile(const string& path) {
+    ifstream file(path, ios::binary); //открываем файлы в бинарном режиме, чтобы ничего не сломалось
+    if (!file.is_open()) {
+        throw runtime_error("Не удалось открыть файл: " + path);
     }
-    cout << endl;
+    //чтение файла в одну строку, итератор ходит по байтам файла, конструктор
+    //проходит по каждому байту файла и складывая каждый байт в строку
+    string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
+    return content;
+}
+
+string readKey(const string& path) {
+    ifstream file(path); //здесь не нужно считываение в бинарном режиме, у нас ключ подается изначально как набор 0 и 1
+    if (!file.is_open()) {
+        throw runtime_error("Не удалось открыть файл: " + path);
+    }
+    string bits;
+    file >> bits;
+    if (bits.size != 8) {
+        throw runtime_error("Ключ должен быть 8 бит!");
+    }
+    unsigned char res = (unsigned char)stoi(bits, nullptr, 2);
+    file.close();
+    return res;
+
+}
+
+unsigned char encryptXOR(unsigned char textByte, unsigned char key) {
+
 }
